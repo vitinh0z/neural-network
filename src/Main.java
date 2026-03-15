c30641dac40b834fbea8f08dd146dd788ea2b5d6
vitinh0z <victor10.vg23@gmail.com>
2026-03-15 18:28:29 -0300

    feat: implementa rede neural simples em java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import entity.Neural;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        double[] weights = {0.5, 1.0};
+        double bias = 0.5;
+
+        Neural neural = new Neural(weights, bias);
+
+        double[][] inputs = {
+                {2, 1},
+                {3, 1},
+                {4, 1},
+                {5, 1}
+        };
+
+        double[] outputs = {
+                4,
+                6,
+                8,
+                10
+        };
+
+        double learningRate = 0.001;
+
+        for (int epoch = 0; epoch < 10000; epoch++) {
+
+            for (int i = 0; i < inputs.length; i++) {
+                neural.treinar(inputs[i], outputs[i], learningRate);
+            }
+        }
+
+        double[] testInput = {6, 1};
+
+        double resultado = neural.calcular(testInput);
+
+        System.out.println("Resultado após treino: " + resultado);
+
+    }
 }
--- src/entity/Neural.java
+++ src/entity/Neural.java
@@ -1,4 +1,65 @@
 package entity;
 
 public class Neural {
+    private double[] weights;
+    private double bias;
+
+    public Neural (double[] weights, double bias){
+        this.bias = bias;
+        this.weights = weights;
+    }
+
+    public double[] getWeights() {
+        return weights;
+    }
+
+    public void setWeights(double[] weights) {
+        this.weights = weights;
+    }
+
+    public double getBias() {
+        return bias;
+    }
+
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    public double leakyReLu(double x){
+        final double alpha = 0.01;
+        if (x > 0) return x;
+        else return alpha * x;
+    }
+
+    /*
+    * if we have:
+    * input = {5, 30}
+    * weights = {0.8, 0.2}
+    *
+    * the loop make:
+    *
+    * (first interaction): 5 * 0.8 = 4
+    * sum = 4
+    *
+    * (second interaction): 30 * 0.2 = 6
+    * sum = 4 + 6 = 10
+    * */
+    public double calcular (double[] inputs){
+        double sum = 0;
+        for (int i = 0; i< inputs.length; i++){
+            sum += weights[i] * inputs[i];
+        }
+        sum += bias;
+        return leakyReLu(sum);
+    }
+
+    public void treinar (double[] inputs, double waited, double learningRate){
+        double preview = calcular(inputs);
+        double error = waited - preview;
+
+        for (int i = 0; i<weights.length; i++){
+            weights[i] += error * learningRate * inputs[i];
+        }
+        bias += error * learningRate;
+    }
 }
